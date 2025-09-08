e2749bd75a867422ddc11ebf3b36506914c9559c
Deepak <guitarjinny@gmail.com>
2025-09-08 22:12:59 +0530

    dockerize the project

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.MapOpenApi();
+app.MapScalarApiReference();
+//}
 
 app.UseHttpsRedirection();
 
